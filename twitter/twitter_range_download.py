dea67fd859e135e33be3e237a787cb43e9edfeba
Alban Lecocq <alban.lecocq@corp.ovh.com>
2017-04-03 13:51:48 +0200

    1st commit

---
+++ twitter/twitter_range_download.py
@@ -0,0 +1,84 @@
+# To run this code, first edit config.py with your configuration, then:
+#
+# mkdir data
+# python twitter_stream_download.py -q apple -d data
+# 
+# It will produce the list of tweets for the query "apple" 
+# in the file data/stream_apple.json
+
+import tweepy
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import time
+import argparse
+import string
+import config
+import json
+
+def get_parser():
+    """Get parser for command line arguments."""
+    parser = argparse.ArgumentParser(description="Twitter Downloader")
+    parser.add_argument("-q",
+                        "--query",
+                        dest="query",
+                        help="Query/Filter",
+                        default='-')
+    parser.add_argument("-d",
+                        "--data-dir",
+                        dest="data_dir",
+                        help="Output/Data Directory")
+    return parser
+
+
+
+
+def format_filename(fname):
+    """Convert file name into a safe string.
+
+    Arguments:
+        fname -- the file name to convert
+    Return:
+        String -- converted file name
+    """
+    return ''.join(convert_valid(one_char) for one_char in fname)
+
+
+def convert_valid(one_char):
+    """Convert a character into '_' if invalid.
+
+    Arguments:
+        one_char -- the char to convert
+    Return:
+        Character -- converted char
+    """
+    valid_chars = "-_.%s%s" % (string.ascii_letters, string.digits)
+    if one_char in valid_chars:
+        return one_char
+    else:
+        return '_'
+
+
+def process_or_store(tweet):
+    print(json.dumps(tweet._json))
+
+
+@classmethod
+def parse(cls, api, raw):
+    status = cls.first_parse(api, raw)
+    setattr(status, 'json', json.dumps(raw))
+    return status
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+    auth = OAuthHandler(config.consumer_key, config.consumer_secret)
+    auth.set_access_token(config.access_token, config.access_secret)
+    api = tweepy.API(auth)
+
+    for tweet in tweepy.Cursor(api.search,
+                           q=args.query,
+                           since="2017-03-28",
+                           until="2017-03-29").items():
+      process_or_store(tweet)
+
